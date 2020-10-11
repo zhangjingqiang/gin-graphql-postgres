0b50bb886c58df81332189c66a18c1d7463a2341
zhangjingqiang <zhangjingqiang@gmail.com>
2020-10-11 20:30:49 +0900

    add graphql

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gin-graphql-postgres
+
+## Usage
+
+### Set database
+
+```
+$ sudo -i -u postgres
+$ createdb test
+$ psql test < db/dump
+```
+
+### Run server
+
+```
+$ go run server.go
+```
+
+## License
+
+MIT
---
+++ db/main.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/zhangjingqiang/gin-graphql-postgres/graph/model"
+)
+
+type dbConfig struct {
+	host     string
+	port     int
+	user     string
+	dbname   string
+	password string
+}
+
+var config = dbConfig{"localhost", 5432, "postgres", "test", "root"}
+
+func getDatabaseUrl() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s dbname=%s password=%s",
+		config.host, config.port, config.user, config.dbname, config.password)
+}
+
+func GetDatabase() (*gorm.DB, error) {
+	db, err := gorm.Open("postgres", getDatabaseUrl())
+	// RunMigrations(db)
+	return db, err
+}
+
+func RunMigrations(db *gorm.DB) {
+	if !db.HasTable(&model.Question{}) {
+		db.CreateTable(&model.Question{})
+	}
+	if !db.HasTable(&model.Choice{}) {
+		db.CreateTable(&model.Choice{})
+		db.Model(&model.Choice{}).AddForeignKey("question_id", "questions(id)", "CASCADE", "CASCADE")
+	}
+}
